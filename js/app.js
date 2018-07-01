247fb795691ff94c189e80f7a9fd55378f4dc257
Darlan Caruso <devcaruso@gmail.com>
2018-07-01 13:12:28 -0300

    Criando vetor de cartas

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 var contador = document.querySelector('#movimentos');
+var cartas = [
+    { id: 0, icone: "fas fa-code" },
+    { id: 1, icone: "fab fa-codepen" },
+    { id: 2, icone: "fas fa-code-branch" },
+    { id: 3, icone: "fab fa-free-code-camp" },
+    { id: 4, icone: "fas fa-terminal" },
+    { id: 5, icone: "fab fa-css3" },
+    { id: 6, icone: "fas fa-qrcode" },
+    { id: 7, icone: "fab fa-linode" },
+];
+var todasCartas = cartas.concat(cartas);
+todasCartas.sort();
+console.log(todasCartas);
 
 function adicionarContador() {
     contador.innerHTML++;
