9c717d7fc604ad242d9e707d81cba5ef6b323b5b
Darlan Caruso <devcaruso@gmail.com>
2018-07-01 13:20:09 -0300

    Retornando cartas em posições aleatórias

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var contador = document.querySelector('#movimentos');
-var cartas = [
+var vetorCartas = [
     { id: 0, icone: "fas fa-code" },
     { id: 1, icone: "fab fa-codepen" },
     { id: 2, icone: "fas fa-code-branch" },
@@ -9,15 +9,36 @@ var cartas = [
     { id: 6, icone: "fas fa-qrcode" },
     { id: 7, icone: "fab fa-linode" },
 ];
-var todasCartas = cartas.concat(cartas);
-todasCartas.sort();
-console.log(todasCartas);
+var cartas = vetorCartas.concat(vetorCartas);
+
+//Adiciona o contador
 
 function adicionarContador() {
     contador.innerHTML++;
 };
 
+//Reinicia o jogo
+
 function reiniciarJogo() {
     contador.innerHTML = 0;
 };
 
+//Retorna as cartas em posições aleatórias
+
+function retornarCartas(cartas) {
+    var index = cartas.length, valorTemporario, indexAleatorio;
+
+    while(0 != index) {
+        indexAleatorio = Math.floor(Math.random() * index);
+        index -= 1;
+
+        valorTemporario = cartas[index];
+        cartas[index] = cartas[indexAleatorio];
+        cartas[indexAleatorio] = valorTemporario;
+    }
+
+    return cartas;
+};
+
+cartas = retornarCartas(cartas);
+console.log(cartas);
