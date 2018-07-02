1d26a05906ddab1caedc7330633398809c9ebd14
Darlan Caruso <devcaruso@gmail.com>
2018-07-01 21:12:41 -0300

    Adicionando função para reduzir trapaças no jogo

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 //Contador
+
 var contador = document.querySelector('#movimentos');
 var resultado = 0;
 
 //Container
+
 var container = document.querySelector('#cartas');
 
 //Lista de cartas
+
 var vetorCartas = [
     { id: 0, icone: "fa-angular" },
     { id: 1, icone: "fa-codepen" },
@@ -18,12 +21,15 @@ var vetorCartas = [
 ];
 
 //Duplica as cartas
+
 var cartas = vetorCartas.concat(vetorCartas);
 
 //Retorna o vetor em posições aleatórias
+
 cartas = retornarCartas(cartas);
 
 //Verifica se a carta selecionada é a mesma ou não
+
 function matchingGame(colunas) {
     var cartaEscolhida = [];
     colunas.forEach(coluna => {
@@ -36,7 +42,7 @@ function matchingGame(colunas) {
                 if (cartaEscolhida[0].id === cartaEscolhida[1].id && cartaEscolhida[0].posicao !== cartaEscolhida[1].posicao) {
                     toggleClass(cartaEscolhida, true);
                     resultado++;
-                    if(resultado === 1) {
+                    if(resultado === 8) {
                         window.scrollTo(0,0);
                         document.querySelector('body').setAttribute('style', 'overflow-y: hidden');
                         document.querySelector('#resultado').setAttribute('style', 'display: block');
@@ -52,16 +58,15 @@ function matchingGame(colunas) {
 };
 
 //Adicona a classe referente ao acerto ou erro
+
 function toggleClass(vetorEscolhido, acerto) {
     if(acerto) {
         document.querySelector(`#${vetorEscolhido[0].posicao.toString()} .flip-container`).classList.add('hover');
         document.querySelector(`#${vetorEscolhido[0].posicao.toString()} .flip-container .back`).classList.add('opcao-correta');
         document.querySelector(`#${vetorEscolhido[0].posicao.toString()} .flip-container .back`).setAttribute('disabled', 'disabled');
-        document.querySelector(`#${vetorEscolhido[0].posicao.toString()}`).removeEventListener('click', () => { });
         document.querySelector(`#${vetorEscolhido[1].posicao.toString()} .flip-container`).classList.add('hover');
         document.querySelector(`#${vetorEscolhido[1].posicao.toString()} .flip-container .back`).classList.add('opcao-correta');
         document.querySelector(`#${vetorEscolhido[1].posicao.toString()} .flip-container .back`).setAttribute('disabled', 'disabled');
-        document.querySelector(`#${vetorEscolhido[1].posicao.toString()}`).removeEventListener('click', () => { });
     } else {
         document.querySelector(`#${vetorEscolhido[0].posicao.toString()} .flip-container`).classList.add('hover');
         document.querySelector(`#${vetorEscolhido[0].posicao.toString()} .flip-container .back`).classList.add('opcao-incorreta');
@@ -79,6 +84,7 @@ function toggleClass(vetorEscolhido, acerto) {
 }
 
 //Cria os elementos do DOM
+
 function renderizarElementos() {
     while (container.hasChildNodes()) {
         container.removeChild(container.firstChild);
@@ -129,11 +135,13 @@ function renderizarElementos() {
 };
 
 //Adiciona o contador
+
 function adicionarContador() {
     contador.innerHTML++;
 };
 
 //Reinicia o jogo
+
 function reiniciarJogo() {
     contador.innerHTML = 0;
     cartas = retornarCartas(cartas);
@@ -143,6 +151,7 @@ function reiniciarJogo() {
 };
 
 //Retorna as cartas em posições aleatórias
+
 function retornarCartas(cartas) {
     var index = cartas.length, valorTemporario, indexAleatorio;
 
@@ -159,3 +168,31 @@ function retornarCartas(cartas) {
 };
 
 renderizarElementos();
+
+//Protege o jogo contra trapaças
+
+function retornaAviso() {
+    window.scrollTo(0, 0);
+    document.querySelector('body').setAttribute('style', 'overflow-y: hidden');
+    document.querySelector('#aviso').setAttribute('style', 'display: block');
+};
+
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+        retornaAviso();
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+        retornaAviso();
+        return false;
+    }
+});
+
+$(document).on("contextmenu", function (e) {
+    e.preventDefault();
+    retornaAviso();
+});
+
+function fecharAviso() {
+    document.querySelector('body').setAttribute('style', 'overflow-y: scrol');
+    document.querySelector('#aviso').setAttribute('style', 'display: none');
+};
